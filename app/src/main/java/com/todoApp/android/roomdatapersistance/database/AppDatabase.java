0650565e016206403ed0061544b1c18795f017cb
Rakshya Moktan <moktanrakshya1@gmail.com>
2022-05-29 11:16:35 +0545

    Configured Room Database

--- app/src/main/java/com/todoApp/android/roomdatapersistance/database/AppDatabase.java
+++ app/src/main/java/com/todoApp/android/roomdatapersistance/database/AppDatabase.java
@@ -10,6 +10,7 @@ import androidx.room.TypeConverters;
 
 @Database(entities = {TaskEntry.class, CategoryEntry.class}, version = 1, exportSchema = false)
 @TypeConverters(DateConverter.class)
+//Database Login and Instance
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String LOG_TAG = AppDatabase.class.getSimpleName();
